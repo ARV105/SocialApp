7accccc8c8c3e45bcf431e5d4325afe2323254be
ARV105 <aamadeus2424@gmail.com>
2017-06-12 19:44:15 -0700

    SignInView
    Add signIn view in storyboard with assets

--- SocialApp/SignInVC.swift
+++ SocialApp/SignInVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignInVC.swift
 //  SocialApp
 //
 //  Created by Auriel on 6/12/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignInVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
